f25d7b7be73132501512192e1d4529d31137a920
DevIsis <kitcher96@gmail.com>
2023-06-26 18:24:45 +0000

    Create add new task functionality

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 const taskList = document.querySelector("#task-list");
-taskList.style.color = 'yellow';
+const task = document.querySelector("#task");
+
+addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    //Access form data
+    const newTask = task.value;
+
+    
+    //Create validation for identical item in list & empty string
+    if (newTask.trim() === ''){
+        alert('Task can not be empty.');
+        return;
+    }
+    //Create new task item
+    const taskItem = document.createElement('li');
+    taskItem.textContent = newTask;
+
+    //Post task to task list
+    taskList.appendChild(taskItem);
+
+    //Clear input box
+    task.value = '';
+ });
