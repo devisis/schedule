173890183654e0a99473da0850fefef06555b6bb
DevIsis <kitcher96@gmail.com>
2023-07-05 10:10:24 +0000

    Add update button without functionality and update button styling

--- app.js
+++ app.js
@@ -7,33 +7,48 @@ addEventListener('submit', function (event) {
     //Access form data
     const newTask = task.value;
 
-    
     //Create validation for identical item in list & empty string
-    if (newTask.trim() === ''){
+    if (newTask.trim() === '') {
         alert('Task cannot be empty.');
         return;
     }
 
     //Create new task item
     const taskItem = document.createElement('li');
+    taskItem.setAttribute('class', 'taskContainer d-flex justify-content-between align-items-center')
+
     taskItem.textContent = newTask;
-
-    //Create new delete button
-    const deleteItem = document.createElement('button');
-    deleteItem.setAttribute('class', 'deleteBtn')
-    deleteItem.textContent = 'delete';
-    deleteItem.addEventListener('click', ()=> {
-        // Remove  task item  when delete button is pressed
-        taskList.removeChild(taskItem)
-    });
-    
-    //Append delete button to list item
-    taskItem.appendChild(deleteItem);
-
 
     //Post task to task list
     taskList.appendChild(taskItem);
 
+    //Create new delete button
+    const deleteItem = document.createElement('button');
+    deleteItem.setAttribute('class', 'delete-btn')
+    deleteItem.textContent = 'delete';
+    deleteItem.addEventListener('click', () => {
+        // Remove  task item  when delete button is pressed
+        taskList.removeChild(taskItem)
+    });
+
+    //Append delete button to list item
+    taskItem.appendChild(deleteItem);
+
+    //Create new update button
+    let updateButtons = document.querySelectorAll('.update-btn')
+    const updateItem = document.createElement('button');
+    updateItem.setAttribute('class', 'update-btn')
+    updateItem.textContent = 'edit';
+    updateItem.addEventListener('click', () => {
+        // update  task item  when update button is pressed
+        
+        updateItem.textContent = 'update';
+    });
+
+    //Append delete button to list item
+    taskItem.appendChild(updateItem);
+
+
     //Clear input box
     task.value = '';
- });
+});
